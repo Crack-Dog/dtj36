a79ebf060c1ed80011b5a10f58a4b40ed216338e
Wesley Mak <wesleymak005@gmail.com>
2024-06-29 12:22:39 -0400

    add jump logic (ground check)

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -7,8 +7,12 @@ public class MovementScript : MonoBehaviour
 {
     public float playerSpeed = 10;
     public float playerJump = 10;
+
     public Rigidbody2D rb;
+
     private float Move;
+
+    public bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +27,25 @@ public class MovementScript : MonoBehaviour
         Move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(playerSpeed * Move, rb.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
         {
             rb.velocity += Vector2.up * playerJump;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJumping = true;
         }
     }
 }
